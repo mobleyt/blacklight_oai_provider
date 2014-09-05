c8c5a8556b067e7b6d1bf80ee539910ca34d04af
Tyler Mobley <mobleyt@cofc.edu>
2014-09-05 13:41:28 -0400

    Update solr_document_wrapper.rb
    specify alternate qt

--- lib/blacklight_oai_provider/solr_document_wrapper.rb
+++ lib/blacklight_oai_provider/solr_document_wrapper.rb
@@ -16,30 +16,30 @@ module BlacklightOaiProvider
     end
     
     def earliest
-      Time.parse @controller.get_search_results(@controller.params, {:fl => @timestamp_field, :sort => @timestamp_field +' asc', :rows => 1}).last.first.get(@timestamp_field)
+      Time.parse @controller.get_search_results(@controller.params, {:qt => 'oai', :fl => @timestamp_field, :sort => @timestamp_field +' asc', :rows => 1}).last.first.get(@timestamp_field)
     end
 
     def latest
-      Time.parse @controller.get_search_results(@controller.params, {:fl => @timestamp_field, :sort => @timestamp_field +' desc', :rows => 1}).last.first.get(@timestamp_field)
+      Time.parse @controller.get_search_results(@controller.params, {:qt => 'oai', :fl => @timestamp_field, :sort => @timestamp_field +' desc', :rows => 1}).last.first.get(@timestamp_field)
     end
 
     def find(selector, options={})
       return next_set(options[:resumption_token]) if options[:resumption_token]
 
       if :all == selector
-        response, records = @controller.get_search_results(@controller.params, {:sort => @timestamp_field + ' asc', :rows => @limit})
+        response, records = @controller.get_search_results(@controller.params, {:qt => 'oai', :sort => @timestamp_field + ' asc', :rows => @limit})
 
         if @limit && response.total >= @limit
           return select_partial(OAI::Provider::ResumptionToken.new(options.merge({:last => 0})))
         end
       else                                                    
-        records = @controller.get_search_results(@controller.params, {:phrase_filters => {:id => selector.split('/', 2).last}}).last.first
+        records = @controller.get_search_results(@controller.params, {:qt => 'oai', :phrase_filters => {:id => selector.split('/', 2).last}}).last.first
       end
       records
     end
 
     def select_partial token
-      records = @controller.get_search_results(@controller.params, {:sort => @timestamp_field + ' asc', :rows => @limit, :start => token.last}).last
+      records = @controller.get_search_results(@controller.params, {:qt => 'oai', :sort => @timestamp_field + ' asc', :rows => @limit, :start => token.last}).last
 
       raise ::OAI::ResumptionTokenException.new unless records
 
